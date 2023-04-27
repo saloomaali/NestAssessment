5725b44443795b27b7b97c2f1634b3ec902aaf57
Salooma <saloomaali2000@gmail.com>
2023-04-27 14:13:12 +0530

    viewAllTasks Implemented

--- src/main/java/com/nestAssesment/taskManager/TaskManager/controller/MyTaskController.java
+++ src/main/java/com/nestAssesment/taskManager/TaskManager/controller/MyTaskController.java
@@ -1,9 +1,11 @@
 package com.nestAssesment.taskManager.TaskManager.controller;
-
-import com.nestAssesment.taskManager.TaskManager.entity.Task;
 import com.nestAssesment.taskManager.TaskManager.repository.TaskRepository;
+import com.nestAssesment.taskManager.TaskManager.service.MyTaskService;
+import com.nestAssesment.taskManager.TaskManager.service.MyTaskServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,13 +14,23 @@ import java.util.Map;
 @RestController
 public class MyTaskController {
 
-    TaskRepository taskRepository;
+    @Autowired
+    MyTaskServiceImpl myTaskServiceImpl;
+
+
 
 @CrossOrigin(origins = "*")
 @GetMapping("/viewAllTasks")
 public List<Map<String, String>> viewTasks(){
 
-    return taskRepository.viewAllTasks();
+    return myTaskServiceImpl.viewAllTask();
+}
+
+@CrossOrigin(origins = "*")
+@PostMapping(path = "/updateComment", consumes = "application/json",produces = "application/json")
+public Map<String, String> updateComment(){
+
+    return null;
 }
 
 }
--- src/main/java/com/nestAssesment/taskManager/TaskManager/repository/TaskRepository.java
+++ src/main/java/com/nestAssesment/taskManager/TaskManager/repository/TaskRepository.java
@@ -1,16 +1,21 @@
 package com.nestAssesment.taskManager.TaskManager.repository;
 
 import com.nestAssesment.taskManager.TaskManager.entity.Task;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Map;
 
-public interface TaskRepository extends CrudRepository<Task, Integer> {
+public interface TaskRepository extends JpaRepository<Task, Integer> {
 
-        @Query(value = "SELECT `description`, `comment`, `status`, `due_date`, `patient_id`, `deleted`, p.facility, p.id,p.name, p.room,p.bed, p.unit_name FROM `task` JOIN patient p ON p.id = patient_id WHERE deleted = FALSE ", nativeQuery = true)
+
+        @Query(value = "SELECT t.description, t.comment, t.status, t.priority, t.due_date, t.patient_id, t.deleted, p.id, p.name, p.location, p.facility, p.unit_name, p.room, p.bed FROM task t JOIN patient p ON patient_id = p.id WHERE t.deleted = 0 ", nativeQuery = true)
         List<Map<String, String>> viewAllTasks();
+
 
     }
 
